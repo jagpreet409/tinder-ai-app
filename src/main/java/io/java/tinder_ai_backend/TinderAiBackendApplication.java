417ecabba1b80d7fc1567eb89da654427a18c512
Jagpreet Singh <jagpreet409@gmail.com>
2024-10-13 20:18:00 +1000

    Add profile to database and checking MongoDB connection

--- src/main/java/io/java/tinder_ai_backend/TinderAiBackendApplication.java
+++ src/main/java/io/java/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,13 +1,32 @@
 package io.java.tinder_ai_backend;
 
+import io.java.tinder_ai_backend.profiles.Gender;
+import io.java.tinder_ai_backend.profiles.Profile;
+import io.java.tinder_ai_backend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+	public void run(String[] args){
+		Profile profile = new Profile("1","Jagpreet","Singh",40,
+				"Indian", Gender.MALE,"Software Engineer",
+				"foo.jpg", "INIP");
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);
+
+		System.out.println("My App is running");
 	}
 
 }
